042c746a91709d8bcd63f20ed94fb31779ab4ec1
rebeca <>
2012-04-04 14:36:23 +0000

    fixing merge.sh

--- SingleTop-tW/scripts/merge.sh
+++ SingleTop-tW/scripts/merge.sh
@@ -1,3 +1,4 @@
+rm outputs/*zjetsall.root;
 hadd -f outputs/out_0_zjetsall.root outputs/out_0_zjets*;
 hadd -f outputs/out_1_zjetsall.root outputs/out_1_zjets*;
 hadd -f outputs/out_2_zjetsall.root outputs/out_2_zjets*;
@@ -14,9 +15,9 @@ hadd -f outputs/out_0_st.root outputs/out_0_t.root outputs/out_0_at.root outputs
 hadd -f outputs/out_1_st.root outputs/out_1_t.root outputs/out_1_at.root outputs/out_1_ts.root outputs/out_1_ats.root;
 hadd -f outputs/out_2_st.root outputs/out_2_t.root outputs/out_2_at.root outputs/out_2_ts.root outputs/out_2_ats.root;
 
-hadd -f  outputs/out_0_di.root outputs/out_0_ww.root outputs/out_0_wz.root outputs/out_0_zz.root ;
-hadd -f  outputs/out_1_di.root outputs/out_1_ww.root outputs/out_1_wz.root outputs/out_1_zz.root ;
-hadd -f  outputs/out_2_di.root outputs/out_2_ww.root outputs/out_2_wz.root outputs/out_2_zz.root ;
+hadd -f outputs/out_0_di.root outputs/out_0_ww.root outputs/out_0_wz.root outputs/out_0_zz.root ;
+hadd -f outputs/out_1_di.root outputs/out_1_ww.root outputs/out_1_wz.root outputs/out_1_zz.root ;
+hadd -f outputs/out_2_di.root outputs/out_2_ww.root outputs/out_2_wz.root outputs/out_2_zz.root ;
 
 hadd -f outputs/out_0_others.root outputs/out_0_di.root outputs/out_0_st.root outputs/out_0_wjets.root outputs/out_0_qcd_mu.root ;
 hadd -f outputs/out_1_others.root outputs/out_1_di.root outputs/out_1_st.root outputs/out_1_wjets.root outputs/out_1_qcd_mu.root ;
