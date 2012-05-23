4dc3cb65ea7e6904135ebdd6267c938f9ebdc8af
ghammad <>
2012-05-23 09:19:56 +0000

    Updated version

--- TopFCNC/macros/CompileAndRun.sh
+++ TopFCNC/macros/CompileAndRun.sh
@@ -1,23 +1,26 @@
 #!/bin/bash   
 if [ "${1}" != "" ]; then
 
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/lib
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:${HOME}/lib:../../TMVA/lib
 
   #echo ${LD_LIBRARY_PATH}
-
+  LDFLAGS=`root-config --ldflags`
+  
   MACRO=$1
 
   EXECUTABLE=`echo ${1} | cut -d'.' -f1`
     
   echo "Will compile macro ${MACRO}"
 
-  g++ -m32 -I../../../ -I../../ -I.. -L${HOME}/lib -lTopTreeAnaContent42 -lTopTreeAna42 -I `root-config --incdir` `root-config --libs` ${MACRO} -o ${EXECUTABLE}
+  #g++ -m32 -I../../../ -I../../ -I.. -L${HOME}/lib -lTopTreeAnaContent42 -lTopTreeAna42 -I `root-config --incdir` `root-config --libs` ${MACRO} -o ${EXECUTABLE}
+  g++ ${LDFLAGS} -I../../../ -I../../ -I.. -L${HOME}/lib -lTopFcncAnalysis42 -I `root-config --incdir` `root-config --libs` ${MACRO} -o ${EXECUTABLE}
 
-  echo "Done. Will now run the associated executable ${EXECUTABLE}"
-
+  if [ -e ${EXECUTABLE} ]; then
+    echo "Done. Will now run the associated executable ${EXECUTABLE}"
   #./TopFCNC_EventSelection | tee $2
+  else
+    echo "Compilation failed!"
+  fi
 else
   echo "Please, specify a macro (ex : foo.cc)"
 fi
