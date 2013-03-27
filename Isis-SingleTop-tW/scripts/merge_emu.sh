0345be9be909902ff982da8c7c895a523e05fe08
ivanpari <>
2013-03-27 17:03:54 +0000

    *** empty log message ***

--- Isis-SingleTop-tW/scripts/merge_emu.sh
+++ Isis-SingleTop-tW/scripts/merge_emu.sh
@@ -11,7 +11,7 @@ hadd -f outputs/out_0_mc.root outputs/out_0_twdr.root outputs/out_0_tt.root outp
 rm outputs/JERsysDown_*_zjetsall.root;
 hadd -f outputs/JERsysDown_0_zjetsall.root outputs/JERsysDown_0_zjets*;
 hadd -f outputs/JERsysDown_0_twdr.root outputs/JERsysDown_0_tw_dr.root outputs/JERsysDown_0_atw_dr.root;
-hadd -f outputs/JERsysDown_0_twds.root outputs/JERsysDown_0_tw_ds.root outputs/JERsysDown_0_atw_ds.root;
+
 hadd -f outputs/JERsysDown_0_st.root outputs/JERsysDown_0_t.root outputs/JERsysDown_0_at.root outputs/JERsysDown_0_s.root outputs/JERsysDown_0_as.root;
 hadd -f outputs/JERsysDown_0_di.root outputs/JERsysDown_0_ww.root outputs/JERsysDown_0_wz.root outputs/JERsysDown_0_zz.root ;
 hadd -f outputs/JERsysDown_0_others.root outputs/JERsysDown_0_di.root outputs/JERsysDown_0_st.root outputs/JERsysDown_0_wjets.root  ;
@@ -21,7 +21,7 @@ hadd -f outputs/JERsysDown_0_mc.root outputs/JERsysDown_0_twdr.root outputs/JERs
 rm outputs/JERsysUp_*_zjetsall.root;
 hadd -f outputs/JERsysUp_0_zjetsall.root outputs/JERsysUp_0_zjets*;
 hadd -f outputs/JERsysUp_0_twdr.root outputs/JERsysUp_0_tw_dr.root outputs/JERsysUp_0_atw_dr.root;
-hadd -f outputs/JERsysUp_0_twds.root outputs/JERsysUp_0_tw_ds.root outputs/JERsysUp_0_atw_ds.root;
+
 hadd -f outputs/JERsysUp_0_st.root outputs/JERsysUp_0_t.root outputs/JERsysUp_0_at.root outputs/JERsysUp_0_s.root outputs/JERsysUp_0_as.root;
 hadd -f outputs/JERsysUp_0_di.root outputs/JERsysUp_0_ww.root outputs/JERsysUp_0_wz.root outputs/JERsysUp_0_zz.root ;
 hadd -f outputs/JERsysUp_0_others.root outputs/JERsysUp_0_di.root outputs/JERsysUp_0_st.root outputs/JERsysUp_0_wjets.root  ;
@@ -30,7 +30,7 @@ hadd -f outputs/JERsysUp_0_mc.root outputs/JERsysUp_0_twdr.root outputs/JERsysUp
 rm outputs/JESsysDown_*_zjetsall.root;
 hadd -f outputs/JESsysDown_0_zjetsall.root outputs/JESsysDown_0_zjets*;
 hadd -f outputs/JESsysDown_0_twdr.root outputs/JESsysDown_0_tw_dr.root outputs/JESsysDown_0_atw_dr.root;
-hadd -f outputs/JESsysDown_0_twds.root outputs/JESsysDown_0_tw_ds.root outputs/JESsysDown_0_atw_ds.root;
+
 hadd -f outputs/JESsysDown_0_st.root outputs/JESsysDown_0_t.root outputs/JESsysDown_0_at.root outputs/JESsysDown_0_s.root outputs/JESsysDown_0_as.root;
 hadd -f outputs/JESsysDown_0_di.root outputs/JESsysDown_0_ww.root outputs/JESsysDown_0_wz.root outputs/JESsysDown_0_zz.root ;
 hadd -f outputs/JESsysDown_0_others.root outputs/JESsysDown_0_di.root outputs/JESsysDown_0_st.root outputs/JESsysDown_0_wjets.root  ;
@@ -40,7 +40,7 @@ hadd -f outputs/JESsysDown_0_mc.root outputs/JESsysDown_0_twdr.root outputs/JESs
 rm outputs/JESsysUp_*_zjetsall.root;
 hadd -f outputs/JESsysUp_0_zjetsall.root outputs/JESsysUp_0_zjets*;
 hadd -f outputs/JESsysUp_0_twdr.root outputs/JESsysUp_0_tw_dr.root outputs/JESsysUp_0_atw_dr.root;
-hadd -f outputs/JESsysUp_0_twds.root outputs/JESsysUp_0_tw_ds.root outputs/JESsysUp_0_atw_ds.root;
+
 hadd -f outputs/JESsysUp_0_st.root outputs/JESsysUp_0_t.root outputs/JESsysUp_0_at.root outputs/JESsysUp_0_s.root outputs/JESsysUp_0_as.root;
 hadd -f outputs/JESsysUp_0_di.root outputs/JESsysUp_0_ww.root outputs/JESsysUp_0_wz.root outputs/JESsysUp_0_zz.root ;
 hadd -f outputs/JESsysUp_0_others.root outputs/JESsysUp_0_di.root outputs/JESsysUp_0_st.root outputs/JESsysUp_0_wjets.root  ;
@@ -59,7 +59,7 @@ hadd -f outputs/PUsysDown_0_mc.root outputs/PUsysDown_0_twdr.root outputs/PUsysD
 rm outputs/PUsysUp_*_zjetsall.root;
 hadd -f outputs/PUsysUp_0_zjetsall.root outputs/PUsysUp_0_zjets*;
 hadd -f outputs/PUsysUp_0_twdr.root outputs/PUsysUp_0_tw_dr.root outputs/PUsysUp_0_atw_dr.root;
-hadd -f outputs/PUsysUp_0_twds.root outputs/PUsysUp_0_tw_ds.root outputs/PUsysUp_0_atw_ds.root;
+
 hadd -f outputs/PUsysUp_0_st.root outputs/PUsysUp_0_t.root outputs/PUsysUp_0_at.root outputs/PUsysUp_0_s.root outputs/PUsysUp_0_as.root;
 hadd -f outputs/PUsysUp_0_di.root outputs/PUsysUp_0_ww.root outputs/PUsysUp_0_wz.root outputs/PUsysUp_0_zz.root ;
 hadd -f outputs/PUsysUp_0_others.root outputs/PUsysUp_0_di.root outputs/PUsysUp_0_st.root outputs/PUsysUp_0_wjets.root  ;
@@ -69,7 +69,7 @@ hadd -f outputs/PUsysUp_0_mc.root outputs/PUsysUp_0_twdr.root outputs/PUsysUp_0_
 rm outputs/SFsysDown_*_zjetsall.root;
 hadd -f outputs/SFsysDown_0_zjetsall.root outputs/SFsysDown_0_zjets*;
 hadd -f outputs/SFsysDown_0_twdr.root outputs/SFsysDown_0_tw_dr.root outputs/SFsysDown_0_atw_dr.root;
-hadd -f outputs/SFsysDown_0_twds.root outputs/SFsysDown_0_tw_ds.root outputs/SFsysDown_0_atw_ds.root;
+
 hadd -f outputs/SFsysDown_0_st.root outputs/SFsysDown_0_t.root outputs/SFsysDown_0_at.root outputs/SFsysDown_0_s.root outputs/SFsysDown_0_as.root;
 hadd -f outputs/SFsysDown_0_di.root outputs/SFsysDown_0_ww.root outputs/SFsysDown_0_wz.root outputs/SFsysDown_0_zz.root ;
 hadd -f outputs/SFsysDown_0_others.root outputs/SFsysDown_0_di.root outputs/SFsysDown_0_st.root outputs/SFsysDown_0_wjets.root  ;
@@ -79,7 +79,7 @@ hadd -f outputs/SFsysDown_0_mc.root outputs/SFsysDown_0_twdr.root outputs/SFsysD
 rm outputs/SFsysUp_*_zjetsall.root;
 hadd -f outputs/SFsysUp_0_zjetsall.root outputs/SFsysUp_0_zjets*;
 hadd -f outputs/SFsysUp_0_twdr.root outputs/SFsysUp_0_tw_dr.root outputs/SFsysUp_0_atw_dr.root;
-hadd -f outputs/SFsysUp_0_twds.root outputs/SFsysUp_0_tw_ds.root outputs/SFsysUp_0_atw_ds.root;
+
 hadd -f outputs/SFsysUp_0_st.root outputs/SFsysUp_0_t.root outputs/SFsysUp_0_at.root outputs/SFsysUp_0_s.root outputs/SFsysUp_0_as.root;
 hadd -f outputs/SFsysUp_0_di.root outputs/SFsysUp_0_ww.root outputs/SFsysUp_0_wz.root outputs/SFsysUp_0_zz.root ;
 hadd -f outputs/SFsysUp_0_others.root outputs/SFsysUp_0_di.root outputs/SFsysUp_0_st.root outputs/SFsysUp_0_wjets.root  ;
@@ -88,7 +88,7 @@ hadd -f outputs/SFsysUp_0_mc.root outputs/SFsysUp_0_twdr.root outputs/SFsysUp_0_
 rm outputs/METsysDown_*_zjetsall.root;
 hadd -f outputs/METsysDown_0_zjetsall.root outputs/METsysDown_0_zjets*;
 hadd -f outputs/METsysDown_0_twdr.root outputs/METsysDown_0_tw_dr.root outputs/METsysDown_0_atw_dr.root;
-hadd -f outputs/METsysDown_0_twds.root outputs/METsysDown_0_tw_ds.root outputs/METsysDown_0_atw_ds.root;
+
 hadd -f outputs/METsysDown_0_st.root outputs/METsysDown_0_t.root outputs/METsysDown_0_at.root outputs/METsysDown_0_s.root outputs/METsysDown_0_as.root;
 hadd -f outputs/METsysDown_0_di.root outputs/METsysDown_0_ww.root outputs/METsysDown_0_wz.root outputs/METsysDown_0_zz.root ;
 hadd -f outputs/METsysDown_0_others.root outputs/METsysDown_0_di.root outputs/METsysDown_0_st.root outputs/METsysDown_0_wjets.root  ;
@@ -98,7 +98,7 @@ hadd -f outputs/METsysDown_0_mc.root outputs/METsysDown_0_twdr.root outputs/METs
 rm outputs/METsysUp_*_zjetsall.root;
 hadd -f outputs/METsysUp_0_zjetsall.root outputs/METsysUp_0_zjets*;
 hadd -f outputs/METsysUp_0_twdr.root outputs/METsysUp_0_tw_dr.root outputs/METsysUp_0_atw_dr.root;
-hadd -f outputs/METsysUp_0_twds.root outputs/METsysUp_0_tw_ds.root outputs/METsysUp_0_atw_ds.root;
+
 hadd -f outputs/METsysUp_0_st.root outputs/METsysUp_0_t.root outputs/METsysUp_0_at.root outputs/METsysUp_0_s.root outputs/METsysUp_0_as.root;
 hadd -f outputs/METsysUp_0_di.root outputs/METsysUp_0_ww.root outputs/METsysUp_0_wz.root outputs/METsysUp_0_zz.root ;
 hadd -f outputs/METsysUp_0_others.root outputs/METsysUp_0_di.root outputs/METsysUp_0_st.root outputs/METsysUp_0_wjets.root  ;
@@ -108,7 +108,7 @@ hadd -f outputs/METsysUp_0_mc.root outputs/METsysUp_0_twdr.root outputs/METsysUp
 rm outputs/TopMassDown_*_zjetsall.root;
 hadd -f outputs/TopMassDown_0_zjetsall.root outputs/TopMassDown_0_zjets*;
 hadd -f outputs/TopMassDown_0_twdr.root outputs/TopMassDown_0_tw_dr.root outputs/TopMassDown_0_atw_dr.root;
-hadd -f outputs/TopMassDown_0_twds.root outputs/TopMassDown_0_tw_ds.root outputs/TopMassDown_0_atw_ds.root;
+
 hadd -f outputs/TopMassDown_0_st.root outputs/TopMassDown_0_t.root outputs/TopMassDown_0_at.root outputs/TopMassDown_0_s.root outputs/TopMassDown_0_as.root;
 hadd -f outputs/TopMassDown_0_di.root outputs/TopMassDown_0_ww.root outputs/TopMassDown_0_wz.root outputs/TopMassDown_0_zz.root ;
 hadd -f outputs/TopMassDown_0_others.root outputs/TopMassDown_0_di.root outputs/TopMassDown_0_st.root outputs/TopMassDown_0_wjets.root  ;
@@ -118,7 +118,7 @@ hadd -f outputs/TopMassDown_0_mc.root outputs/TopMassDown_0_twdr.root outputs/To
 rm outputs/TopMassUp_*_zjetsall.root;
 hadd -f outputs/TopMassUp_0_zjetsall.root outputs/TopMassUp_0_zjets*;
 hadd -f outputs/TopMassUp_0_twdr.root outputs/TopMassUp_0_tw_dr.root outputs/TopMassUp_0_atw_dr.root;
-hadd -f outputs/TopMassUp_0_twds.root outputs/TopMassUp_0_tw_ds.root outputs/TopMassUp_0_atw_ds.root;
+
 hadd -f outputs/TopMassUp_0_st.root outputs/TopMassUp_0_t.root outputs/TopMassUp_0_at.root outputs/TopMassUp_0_s.root outputs/TopMassUp_0_as.root;
 hadd -f outputs/TopMassUp_0_di.root outputs/TopMassUp_0_ww.root outputs/TopMassUp_0_wz.root outputs/TopMassUp_0_zz.root ;
 hadd -f outputs/TopMassUp_0_others.root outputs/TopMassUp_0_di.root outputs/TopMassUp_0_st.root outputs/TopMassUp_0_wjets.root  ;
@@ -127,7 +127,7 @@ hadd -f outputs/TopMassUp_0_mc.root outputs/TopMassUp_0_twdr.root outputs/TopMas
 rm outputs/Q2Down_*_zjetsall.root;
 hadd -f outputs/Q2Down_0_zjetsall.root outputs/Q2Down_0_zjets*;
 hadd -f outputs/Q2Down_0_twdr.root outputs/Q2Down_0_tw_dr.root outputs/Q2Down_0_atw_dr.root;
-hadd -f outputs/Q2Down_0_twds.root outputs/Q2Down_0_tw_ds.root outputs/Q2Down_0_atw_ds.root;
+
 hadd -f outputs/Q2Down_0_st.root outputs/Q2Down_0_t.root outputs/Q2Down_0_at.root outputs/Q2Down_0_s.root outputs/Q2Down_0_as.root;
 hadd -f outputs/Q2Down_0_di.root outputs/Q2Down_0_ww.root outputs/Q2Down_0_wz.root outputs/Q2Down_0_zz.root ;
 hadd -f outputs/Q2Down_0_others.root outputs/Q2Down_0_di.root outputs/Q2Down_0_st.root outputs/Q2Down_0_wjets.root  ;
@@ -137,7 +137,7 @@ hadd -f outputs/Q2Down_0_mc.root outputs/Q2Down_0_twdr.root outputs/Q2Down_0_tt.
 rm outputs/Q2Up_*_zjetsall.root;
 hadd -f outputs/Q2Up_0_zjetsall.root outputs/Q2Up_0_zjets*;
 hadd -f outputs/Q2Up_0_twdr.root outputs/Q2Up_0_tw_dr.root outputs/Q2Up_0_atw_dr.root;
-hadd -f outputs/Q2Up_0_twds.root outputs/Q2Up_0_tw_ds.root outputs/Q2Up_0_atw_ds.root;
+
 hadd -f outputs/Q2Up_0_st.root outputs/Q2Up_0_t.root outputs/Q2Up_0_at.root outputs/Q2Up_0_s.root outputs/Q2Up_0_as.root;
 hadd -f outputs/Q2Up_0_di.root outputs/Q2Up_0_ww.root outputs/Q2Up_0_wz.root outputs/Q2Up_0_zz.root ;
 hadd -f outputs/Q2Up_0_others.root outputs/Q2Up_0_di.root outputs/Q2Up_0_st.root outputs/Q2Up_0_wjets.root  ;
@@ -147,7 +147,7 @@ hadd -f outputs/Q2Up_0_mc.root outputs/Q2Up_0_twdr.root outputs/Q2Up_0_tt.root o
 rm outputs/eleSFsysDown_*_zjetsall.root;
 hadd -f outputs/eleSFsysDown_0_zjetsall.root outputs/eleSFsysDown_0_zjets*;
 hadd -f outputs/eleSFsysDown_0_twdr.root outputs/eleSFsysDown_0_tw_dr.root outputs/eleSFsysDown_0_atw_dr.root;
-hadd -f outputs/eleSFsysDown_0_twds.root outputs/eleSFsysDown_0_tw_ds.root outputs/eleSFsysDown_0_atw_ds.root;
+
 hadd -f outputs/eleSFsysDown_0_st.root outputs/eleSFsysDown_0_t.root outputs/eleSFsysDown_0_at.root outputs/eleSFsysDown_0_s.root outputs/eleSFsysDown_0_as.root;
 hadd -f outputs/eleSFsysDown_0_di.root outputs/eleSFsysDown_0_ww.root outputs/eleSFsysDown_0_wz.root outputs/eleSFsysDown_0_zz.root ;
 hadd -f outputs/eleSFsysDown_0_others.root outputs/eleSFsysDown_0_di.root outputs/eleSFsysDown_0_st.root outputs/eleSFsysDown_0_wjets.root  ;
@@ -157,7 +157,7 @@ hadd -f outputs/eleSFsysDown_0_mc.root outputs/eleSFsysDown_0_twdr.root outputs/
 rm outputs/eleSFsysUp_*_zjetsall.root;
 hadd -f outputs/eleSFsysUp_0_zjetsall.root outputs/eleSFsysUp_0_zjets*;
 hadd -f outputs/eleSFsysUp_0_twdr.root outputs/eleSFsysUp_0_tw_dr.root outputs/eleSFsysUp_0_atw_dr.root;
-hadd -f outputs/eleSFsysUp_0_twds.root outputs/eleSFsysUp_0_tw_ds.root outputs/eleSFsysUp_0_atw_ds.root;
+
 hadd -f outputs/eleSFsysUp_0_st.root outputs/eleSFsysUp_0_t.root outputs/eleSFsysUp_0_at.root outputs/eleSFsysUp_0_s.root outputs/eleSFsysUp_0_as.root;
 hadd -f outputs/eleSFsysUp_0_di.root outputs/eleSFsysUp_0_ww.root outputs/eleSFsysUp_0_wz.root outputs/eleSFsysUp_0_zz.root ;
 hadd -f outputs/eleSFsysUp_0_others.root outputs/eleSFsysUp_0_di.root outputs/eleSFsysUp_0_st.root outputs/eleSFsysUp_0_wjets.root  ;
@@ -166,7 +166,7 @@ hadd -f outputs/eleSFsysUp_0_mc.root outputs/eleSFsysUp_0_twdr.root outputs/eleS
 rm outputs/matchingDown_*_zjetsall.root;
 hadd -f outputs/matchingDown_0_zjetsall.root outputs/matchingDown_0_zjets*;
 hadd -f outputs/matchingDown_0_twdr.root outputs/matchingDown_0_tw_dr.root outputs/matchingDown_0_atw_dr.root;
-hadd -f outputs/matchingDown_0_twds.root outputs/matchingDown_0_tw_ds.root outputs/matchingDown_0_atw_ds.root;
+
 hadd -f outputs/matchingDown_0_st.root outputs/matchingDown_0_t.root outputs/matchingDown_0_at.root outputs/matchingDown_0_s.root outputs/matchingDown_0_as.root;
 hadd -f outputs/matchingDown_0_di.root outputs/matchingDown_0_ww.root outputs/matchingDown_0_wz.root outputs/matchingDown_0_zz.root ;
 hadd -f outputs/matchingDown_0_others.root outputs/matchingDown_0_di.root outputs/matchingDown_0_st.root outputs/matchingDown_0_wjets.root  ;
@@ -176,7 +176,7 @@ hadd -f outputs/matchingDown_0_mc.root outputs/matchingDown_0_twdr.root outputs/
 rm outputs/matchingUp_*_zjetsall.root;
 hadd -f outputs/matchingUp_0_zjetsall.root outputs/matchingUp_0_zjets*;
 hadd -f outputs/matchingUp_0_twdr.root outputs/matchingUp_0_tw_dr.root outputs/matchingUp_0_atw_dr.root;
-hadd -f outputs/matchingUp_0_twds.root outputs/matchingUp_0_tw_ds.root outputs/matchingUp_0_atw_ds.root;
+
 hadd -f outputs/matchingUp_0_st.root outputs/matchingUp_0_t.root outputs/matchingUp_0_at.root outputs/matchingUp_0_s.root outputs/matchingUp_0_as.root;
 hadd -f outputs/matchingUp_0_di.root outputs/matchingUp_0_ww.root outputs/matchingUp_0_wz.root outputs/matchingUp_0_zz.root ;
 hadd -f outputs/matchingUp_0_others.root outputs/matchingUp_0_di.root outputs/matchingUp_0_st.root outputs/matchingUp_0_wjets.root  ;
