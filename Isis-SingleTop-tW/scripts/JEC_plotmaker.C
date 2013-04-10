37c68c099e3992c1dca77664db386b25eca21c18
ivanpari <>
2013-04-10 11:54:27 +0000

    *** empty log message ***

---
+++ Isis-SingleTop-tW/scripts/JEC_plotmaker.C
@@ -0,0 +1,164 @@
+//Isis Van Parijs
+
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TKey.h"
+#include "TFile.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include "setTDRStyle.C"
+//#include "../Tools/interface/MultiSamplePlot.h"
+//#include "../Tools/interface/PlottingTools.h"
+using namespace std;
+
+void JEC_plotmaker(int mode = 0, int region = 0){
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  gStyle->SetErrorX(0);
+  setTDRStyle();
+  gROOT->SetBatch(1);   // to show histos on canvas
+  
+  //Making it pretty
+  labelcms  = new TPaveText(0.12,0.88,0.5,0.94,"NDCBR");
+  labelcms->SetTextAlign(12);
+  labelcms->SetTextSize(0.045);
+  labelcms->SetFillColor(kWhite);
+  labelcms->AddText("CMS Preliminary, #sqrt{s} = 8 TeV");
+  labelcms->SetBorderSize(0);
+
+  labelcms2  = new TPaveText(0.12,0.85,0.5,0.88,"NDCBR");
+  labelcms2->SetTextAlign(12);
+  labelcms2->SetTextSize(0.045);
+  labelcms2->SetFillColor(kWhite);
+     if      (mode == 0) labelcms2->AddText("12 fb^{-1}, e#mu channel  ");	
+    else if (mode == 1) labelcms2->AddText("12 fb^{-1}, #mu#mu channel  "); 	
+    else if (mode == 2) labelcms2->AddText("12 fb^{-1}, ee channel  "); 
+  
+  labelcms2->SetBorderSize(0);
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetPalette(1);
+  
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetCanvasColor(kWhite);
+  gStyle->SetCanvasDefH(600);
+  gStyle->SetCanvasDefW(600);
+  gStyle->SetLabelFont(18,"");
+  
+  gStyle->SetTitleXOffset(1.2);//1.5
+  gStyle->SetTitleYOffset(1.2);//1.7
+  
+  char myRootFile[300];
+  char myRootFile2[300];
+  double lumi = 0; 
+   if      (mode == 0)	 lumi = 11966.617;  	
+    else if (mode == 1) lumi = 12067.294;  	
+    else if (mode == 2) lumi = 12093.792; 
+  
+
+    
+  sprintf(myRootFile,"results/an_%dpb_%d.root", lumi, mode); // take output from looper
+  sprintf(myRootFile2,"results/noJER_an_%dpb_%d.root", lumi, mode); // take output from looper
+ 
+  
+  TFile *_file0 = TFile::Open(myRootFile);
+  cout << myRootFile << endl;
+  TFile *_file2 = TFile::Open(myRootFile2);
+  cout << myRootFile2 << endl;
+  
+  
+  const int nProcess = 1;
+  const int nPlots = 1;
+  TString processName[nProcess] =  { "twdr"};
+  TString processTitle[nProcess] = { "twdr signal"};
+  Color_t color[2] =        { kBlue,kRed};
+  
+  
+  TString cutLabel[nPlots] =     {  "et_jet"};
+  int rebinHisto[nPlots] =       {  4};
+  TString cutTitle[nPlots] =     { "Energy of the jet (GEV)"};
+ 
+
+  TString modeString[3] = {"0","1","2"};
+ 
+  TString plotExtension = "noJER_dis_plot_"; // name of the plots
+  
+  
+
+  
+   
+  
+  
+  //Make plots   
+  TH1F* histo_zjets;
+  TH1F* histo_zjets_NOSF;
+   
+
+   
+  
+   for(int iPlots = 0; iPlots< nPlots; iPlots++)
+   {
+      
+     leg = new TLegend(0.7,0.7,0.94,0.94);
+     leg ->SetFillStyle(1001);
+     leg ->SetFillColor(kWhite);
+     leg ->SetBorderSize(1);
+     
+     
+ 
+	
+	
+     histo_zjets= (TH1F*) _file0->Get(cutLabel[iPlots]+ "_" + processName[0]);
+	       cout << cutLabel[iPlots]+ "_" + processName[0] << endl; 
+	       
+	       
+       histo_zjets->Rebin(rebinHisto[iPlots]);
+               //histo_zjets->SetFillColor(color[1]);
+        histo_zjets->SetLineColor(color[0]);
+        histo_zjets->SetLineWidth(2);
+
+
+       histo_zjetsNOSF= (TH1F*) _file2->Get(cutLabel[iPlots]+ "_" + processName[0]);
+	       cout << cutLabel[iPlots]+ "_" + processName[0] << endl; 
+	       
+	       
+       histo_zjetsNOSF->Rebin(rebinHisto[iPlots]);
+               //histo_zjetsNOSF->SetFillColor(color[1]);
+        histo_zjetsNOSF->SetLineColor(color[1]);
+        histo_zjetsNOSF->SetLineWidth(2); 
+	      
+       leg ->AddEntry(histo_zjets, "twdr signal" , "l");  
+       leg ->AddEntry(histo_zjetsNOSF, "twdr signal - no reweighing" , "l"); 
+       
+      double max = TMath::Max(histo_zjets->GetMaximum(),histo_zjetsNOSF->GetMaximum());
+    
+       TCanvas *c1 = new TCanvas();
+       histo_zjets->Draw("h");
+      histo_zjets->SetMaximum(max * 1.2);
+     //  histo_zjets->SetMinimum(1);
+       histo_zjets->GetXaxis()->SetTitle(cutTitle[iPlots]);
+     
+       
+         histo_zjetsNOSF->Draw("h,sames");
+      histo_zjetsNOSF->SetMaximum(max * 1.2);
+     //  histo_zjetsNOSF->SetMinimum(1);
+       histo_zjetsNOSF->GetXaxis()->SetTitle(cutTitle[iPlots]);
+
+       
+      leg->Draw();
+      labelcms->Draw();
+      labelcms2->Draw();
+    
+      c1->SaveAs("plots/"  + plotExtension + modeString[mode] + "_" + cutLabel[iPlots] + ".png");
+     // c1->SaveAs("plots/pdf/" + plotExtension + modeString[mode] + "_" + cutLabel[iPlots] + ".pdf");
+      
+     
+      
+    } // end plots loop 
+    
+ 
+} // end constructor loop
---
+++ Isis-SingleTop-tW/scripts/pu_plotmaker.C
@@ -0,0 +1,153 @@
+//Isis Van Parijs
+
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TKey.h"
+#include "TFile.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include "setTDRStyle.C"
+//#include "../Tools/interface/MultiSamplePlot.h"
+//#include "../Tools/interface/PlottingTools.h"
+using namespace std;
+
+void pu_plotmaker(){
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  gStyle->SetErrorX(0);
+  setTDRStyle();
+  gROOT->SetBatch(1);   // to show histos on canvas
+  
+
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetPalette(1);
+  
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetCanvasColor(kWhite);
+  gStyle->SetCanvasDefH(600);
+  gStyle->SetCanvasDefW(600);
+  gStyle->SetLabelFont(18,"");
+  
+  gStyle->SetTitleXOffset(1.2);//1.5
+  gStyle->SetTitleYOffset(1.2);//1.7
+  
+  char myRootFile[300];
+  char myRootFile1[300];
+  char myRootFile2[300];
+  char myRootFile3[300];
+
+  
+
+    
+  sprintf(myRootFile,"../pileupHistos/pileup_MC_Summer12.root"); // take output from looper
+   sprintf(myRootFile1,"../pileupHistos/pileup_tW_2012Data53X_UpToRun203002/nominal.root");
+  sprintf(myRootFile2,"../pileupHistos/pileup_tW_2012Data53X_UpToRun203002/sys_down.root"); // take output from looper
+  sprintf(myRootFile3,"../pileupHistos/pileup_tW_2012Data53X_UpToRun203002/sys_up.root");
+  
+  TFile *_file0 = TFile::Open(myRootFile);
+  cout << myRootFile << endl;
+  TFile *_file1 = TFile::Open(myRootFile1);
+  cout << myRootFile1 << endl;
+  TFile *_file2 = TFile::Open(myRootFile2);
+  cout << myRootFile2 << endl;
+  TFile *_file3 = TFile::Open(myRootFile3);
+  cout << myRootFile3<< endl;
+
+  
+  //Make plots   
+  TH1F* histo_puMC;
+ TH1F* histo_pu;
+   TH1F* histo_puUp;
+TH1F* histo_puDown;
+   
+     leg = new TLegend(0.7,0.7,0.94,0.94);
+     leg ->SetFillStyle(1001);
+     leg ->SetFillColor(kWhite);
+     leg ->SetBorderSize(1);
+     
+     
+ 
+	
+	
+     histo_puMC= (TH1F*) _file0->Get("pileup");
+	 
+	       
+	       
+       
+               //histo_puMC->SetFillColor(color[1]);
+        histo_puMC->SetLineColor(kRed);
+        histo_puMC->SetLineWidth(2);
+
+       histo_pu= (TH1F*) _file1->Get("pileup");
+	 
+	       
+	       
+       
+               //histo_pu->SetFillColor(color[1]);
+        histo_pu->SetLineColor(kBlack);
+        histo_pu->SetLineWidth(2);
+	
+	histo_puDown= (TH1F*) _file2->Get("pileup");
+	 
+	       
+	       
+     
+               //histo_puDown->SetFillColor(color[1]);
+        histo_puDown->SetLineColor(kBlue);
+        histo_puDown->SetLineWidth(2);
+	
+	
+	histo_puUp= (TH1F*) _file3->Get("pileup");
+	 
+	       
+	       
+       
+               //histo_puUp->SetFillColor(color[1]);
+        histo_puUp->SetLineColor(kGreen);
+        histo_puUp->SetLineWidth(2);
+	
+
+      
+	      
+       leg ->AddEntry(histo_puMC, "PU MC " , "l");  
+       leg ->AddEntry(histo_pu, "PU data - normal" , "l"); 
+       leg ->AddEntry(histo_puUp, "PU data - SF up" , "l"); 
+       leg ->AddEntry(histo_puDown, "PU data - SF down" , "l"); 
+       
+       
+      double max = TMath::Max(histo_puMC->GetMaximum(),histo_pu->GetMaximum());
+    
+       TCanvas *c1 = new TCanvas();
+       histo_pu->DrawNormalized("h");
+      histo_pu->SetMaximum(max*1.2 );
+     //  histo_puMC->SetMinimum(1);
+       histo_pu->GetXaxis()->SetTitle("Pile-up");
+     
+       
+         histo_puMC->DrawNormalized("h,sames");
+      histo_puMC->SetMaximum(max*1.2 );
+      histo_puMC->GetXaxis()->SetTitle("Pile-up");
+    histo_puUp->DrawNormalized("h,sames");
+      histo_puUp->SetMaximum(max*1.2);
+      histo_puUp->GetXaxis()->SetTitle("Pile-up");
+      histo_puDown->DrawNormalized("h,sames");
+      histo_puDown->SetMaximum(max*1.2 );
+histo_puDown->GetXaxis()->SetTitle("Pile-up");
+       
+      leg->Draw();
+
+    
+      c1->SaveAs("../pileupHistos/pileup.png");
+     // c1->SaveAs("plots/pdf/" + plotExtension + modeString[mode] + "_" + cutLabel[iPlots] + ".pdf");
+      
+     
+      
+  
+    
+ 
+} // end constructor loop
