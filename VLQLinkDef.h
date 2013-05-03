188e18ed4e3eb45160bfc03434917f1bf31b66c4
gvonsem <>
2013-05-03 13:01:37 +0000

    Vector-like quark search linkdef

---
+++ VLQLinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+
+#include "../TopTreeAnalysis/VLQSearch/interface/VLQTree.h"
+
+#else
+
+#include "../TopTreeAnalysis/VLQSearch/interface/VLQTree.h"
+
+
+
+#endif
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class VLQTree;
+
+#endif
