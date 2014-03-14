4c438830eeb07bd54e2cd5499a4aac09a8833b22
ndaci <daci.nadir@gmail.com>
2014-03-14 09:22:18 +0100

    Put all includes in this single file.

---
+++ DarkMatter/Headers.h
@@ -0,0 +1,43 @@
+#define _USE_MATH_DEFINES
+
+// standard library
+#include <iostream>
+#include <map>
+#include <cstdlib> 
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+
+// root library
+#include "TStyle.h"
+#include "TPaveText.h"
+#include "TRandom3.h"
+#include "TRandom.h"
+#include "TMath.h"
+
+// toptree library
+#include "TopTreeProducer/interface/TRootRun.h"
+#include "TopTreeProducer/interface/TRootEvent.h"
+#include "TopTreeAnalysisBase/Selection/interface/SelectionTable.h"
+#include "TopTreeAnalysisBase/Content/interface/AnalysisEnvironment.h"
+#include "TopTreeAnalysisBase/Content/interface/Dataset.h"
+#include "TopTreeAnalysisBase/Tools/interface/JetTools.h"
+#include "TopTreeAnalysisBase/Tools/interface/PlottingTools.h"
+#include "TopTreeAnalysisBase/Tools/interface/MultiSamplePlot.h"
+#include "TopTreeAnalysisBase/Tools/interface/MultiCutPlot.h"
+#include "TopTreeAnalysisBase/Tools/interface/TTreeLoader.h"
+#include "TopTreeAnalysisBase/Tools/interface/AnalysisEnvironmentLoader.h"
+#include "TopTreeAnalysisBase/Reconstruction/interface/JetCorrectorParameters.h"
+#include "TopTreeAnalysisBase/Reconstruction/interface/JetCorrectionUncertainty.h"
+#include "TopTreeAnalysisBase/Reconstruction/interface/MakeBinning.h"
+#include "TopTreeAnalysisBase/MCInformation/interface/LumiReWeighting.h"
+#include "TopTreeAnalysisBase/Reconstruction/interface/MEzCalculator.h"
+#include "TopTreeAnalysisBase/Reconstruction/interface/TTreeObservables.h"
+#include "TopTreeAnalysisBase/Tools/interface/BTagWeightTools.h"
+//#include "TopTreeAnalysisBase/Tools/interface/JetCombiner.h"
+#include "TopTreeAnalysisBase/Tools/interface/MVATrainer.h"
+#include "TopTreeAnalysisBase/Tools/interface/MVAComputer.h"
+
+// custom cms plot style (tdr-like ?)
+#include "TopTreeAnalysis/macros/Style.C"
